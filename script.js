b4181825d1d6ef328407dd6403e2a4e70b68e3ac
Islomjonerkinov <islomjonerkinov13@gmail.com>
2026-04-20 17:57:58 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".btn").onclick = () => {
+  document.getElementById("modal").style.display = "block";
+}
+
+document.querySelector(".close").onclick = () => {
+  document.getElementById("modal").style.display = "none";
+}
